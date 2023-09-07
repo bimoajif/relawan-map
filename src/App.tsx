b3e2b89788dcae1f5fcec5e9614cbba1e337af16
bimoajif <bimoajifajrianto@gmail.com>
2023-09-07 15:03:04 +0700

    complete all projection config

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,34 @@
 import { useState } from 'react'
 import Indonesia from "../src/maps/provinsi/provinces-simplified-topo.json"
 import DataProvinsi from "../src/maps/provinsi/Data/ind-data-prov-rev.json"
-import Aceh from "../src/maps/kabupaten-kota/Jawa Barat/jawa-barat-simplified-topo.json"
+import Aceh from "../src/maps/kabupaten-kota/All/yogyakarta-simplified-topo.json"
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { Button, Pane, SelectMenu, SideSheet } from 'evergreen-ui'
 import { Tooltip as ReactTooltip } from "react-tooltip";
 import { scaleQuantile } from "d3-scale";
+import { COLOR_RANGE, DEFAULT_COLOR } from './assets/color';
 import './App.css'
 
-const COLOR_RANGE = [
-  "#eff6ff",
-  "#e2efff",
-  "#d5e7fe",
-  "#c8e0fe",
-  "#bbd9fd",
-  "#aed1fd",
-  "#a1cafc",
-  "#94c2fc",
-  "#87bbfb",
-  "#7ab4fb",
-  "#6dacfa",
-
-  "#60a5fa",
-  "#599cf7",
-  "#5394f3",
-  "#4c8bf0",
-  "#4582ec",
-  "#3f79e9",
-  "#3871e6",
-  "#3168e2",
-  "#2a5fdf",
-  "#2457db",
-  "#1d4ed8",
-
-  "#1c4acc",
-  "#1944be",
-  "#163eb1",
-  "#1438a3",
-  "#113295",
-  "#0e2c88",
-  "#0b267a",
-  "#08206c",
-  "#061a5e",
-  "#031451",
-  "#000e43",
-];
-
-const DEFAULT_COLOR = "#eff6ff";
+interface Location {
+  id: string | undefined,
+  provinsi: string | undefined,
+  total_relawan: number | undefined,
+  projection_config: {
+    rotate: number[],
+    center: number[],
+    scale: number
+  } | undefined
+}
 
 function App() {
 
   const [tooltipContent, setTooltipContent] = useState("")
   const [isShown, setIsShown] = useState(false)
-  const [selectedProvince, setSelectedProvince] = useState("")
-  const [data, setData] = useState(DataProvinsi)
+  const [selectedProvince, setSelectedProvince] = useState<Location | undefined>(undefined);
+  const [provinceId, setProvinceId] = useState("")
 
   const colorScale = scaleQuantile<string>()
-    .domain(data.map((d) => d.total_relawan))
+    .domain(DataProvinsi.map((d) => d.total_relawan))
     .range(COLOR_RANGE);
 
   return (
@@ -81,7 +53,7 @@ function App() {
             <Geographies geography={Indonesia}>
               {({ geographies }) =>
                 geographies.map((geo) => {
-                  const current = data.find(
+                  const current = DataProvinsi.find(
                     (s) => s.provinsi === geo.properties.provinsi
                   );
                   return (
@@ -94,13 +66,19 @@ function App() {
                       strokeWidth={1}
                       className="animation duration-100 ease-out"
                       onMouseEnter={() => {
-                        setTooltipContent(`${geo.properties.provinsi as string}: ${current?.total_relawan} Relawan`);
+                        setTooltipContent(`${current?.provinsi as string}: ${current?.total_relawan} Relawan`);
                       }}
                       onMouseLeave={() => {
                         setTooltipContent("");
                       }}
                       onClick={() => {
-                        setSelectedProvince(`${geo.properties.provinsi as string}`)
+                        setSelectedProvince({
+                          id: current?.id,
+                          provinsi: current?.provinsi,
+                          total_relawan: current?.total_relawan,
+                          projection_config: current?.projection_config
+                        })
+                        setProvinceId(`${current?.id as string}`)
                         setIsShown(true)
                       }}
                       style={{
@@ -121,10 +99,10 @@ function App() {
               options={DataProvinsi.map((province) => province.provinsi).map((label) => ({ label, value: label }))}
               width={280}
               height={200}
-              selected={selectedProvince}
-              onSelect={(item) => setSelectedProvince(item.value as string)}
+              selected={selectedProvince?.provinsi}
+              // onSelect={(item) => setSelectedProvince(item.value as string)}
             >
-              <Button width={280}>{selectedProvince || 'Pilih Provinsi...'}</Button>
+              <Button width={280}>{selectedProvince?.provinsi || 'Pilih Provinsi...'}</Button>
             </SelectMenu>
             <Button appearance="primary" onClick={() => { setIsShown(true) }}>Cari</Button>
           </div>
@@ -133,42 +111,45 @@ function App() {
             preventBodyScrolling
             onCloseComplete={() => {
               setIsShown(false)
-              setSelectedProvince('')
+              setSelectedProvince(undefined)
             }}
           >
             <Pane className='m-5'>
               <p className='text-slate-500'>Peta Sebaran Jumlah Relawan pada Provinsi</p>
               <h1 className='heading'>
-                {selectedProvince}
+                {selectedProvince?.provinsi}
               </h1>
               <hr />
-              <Pane className='h-[50vh] bg-red-600'>
+              <Pane className='h-[50vh]'>
                 <ComposableMap
                   projection="geoMercator"
                   projectionConfig={{
-                    rotate: [0, 0, 0],
-                    center: [109.7, -8.55],
-                    scale: 6300,
+                    rotate: selectedProvince?.projection_config?.rotate as [number, number, number],
+                    center: selectedProvince?.projection_config?.center as [number, number],
+                    scale: selectedProvince?.projection_config?.scale,
                   }}
                   className="w-screen h-auto"
                 >
-                  <Geographies geography={Aceh}>
+                  <Geographies geography={`../src/maps/kabupaten-kota/All/${selectedProvince?.id}-simplified-topo.json`}>
                     {({ geographies }) =>
-                      geographies.map((geo) => (
-                        <Geography
-                          data-tooltip-id="province-tooltip"
-                          key={geo.rsmKey}
-                          geography={geo}
-                          stroke="#000"
-                          strokeWidth={1}
-                          className="animation duration-100 ease-out"
-                          style={{
-                            default: { fill: "#fff" },
-                            hover: { fill: "#4065F6" },
-                            pressed: { fill: "#4065F6" }
-                          }}
-                        />
-                      ))
+                      geographies.map((geo) => {
+                        console.log(provinceId)
+                        return (
+                          <Geography
+                            data-tooltip-id="province-tooltip"
+                            key={geo.rsmKey}
+                            geography={geo}
+                            stroke="#000"
+                            strokeWidth={1}
+                            className="animation duration-100 ease-out"
+                            style={{
+                              default: { fill: "#fff" },
+                              hover: { fill: "#4065F6" },
+                              pressed: { fill: "#4065F6" }
+                            }}
+                          />
+                        )
+                      })
                     }
                   </Geographies>
                 </ComposableMap>
